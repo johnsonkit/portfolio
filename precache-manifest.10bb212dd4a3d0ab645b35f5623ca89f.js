c1dfb668873d18a9e076b2b8b44698d6ac113f34
Johnson Kit <johnsonkit@users.noreply.github.com>
2020-03-05 19:08:43 +0000

    Updates

---
+++ precache-manifest.10bb212dd4a3d0ab645b35f5623ca89f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4660a3a52dfea774c2f92ba83459e93a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dded18793063cbe57229",
+    "url": "/static/css/main.a93d2028.chunk.css"
+  },
+  {
+    "revision": "983143b55957478437cc",
+    "url": "/static/js/2.7a649cf1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.7a649cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dded18793063cbe57229",
+    "url": "/static/js/main.e36bd3f8.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "3906113f9c4d42d119142b72a23fe220",
+    "url": "/static/media/hero_dt.3906113f.jpg"
+  },
+  {
+    "revision": "e02652dab35c0bd5429de80f01f2f4b8",
+    "url": "/static/media/hero_mb.e02652da.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
